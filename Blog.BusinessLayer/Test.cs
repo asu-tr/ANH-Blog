b922b793455544eaacf7751569634ea20cefac32
asu-tr <29000874+asu-tr@users.noreply.github.com>
2022-03-24 10:11:23 +0300

    test create db if not exists

---
+++ Blog.BusinessLayer/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.BusinessLayer
+{
+    public class Test
+    {
+        DataAccessLayer.BlogDbContext db = new DataAccessLayer.BlogDbContext();
+
+        public Test()
+        {
+           db.Database.CreateIfNotExists();
+        }
+    }
+}
