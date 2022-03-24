c7d3bd7e2152d671833f2d28138064a48a764000
asu-tr <29000874+asu-tr@users.noreply.github.com>
2022-03-24 09:24:06 +0300

    EF added. DbContext added.

---
+++ Blog.DataAccessLayer/BlogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Entities;
+using System.Data.Entity;
+
+namespace Blog.DataAccessLayer
+{
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
